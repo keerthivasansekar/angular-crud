de5a13755c5ac1fd90c5b2ab32eae1d59cc2519f
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-03-27 20:16:08 +0530

    Service Config created

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -8,12 +8,35 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductListComponent implements OnInit {
   
+  dtOptions: DataTables.Settings = {};
+  productList: any;
+  productListSubscribe: any;
+
   constructor(private productService: ProductService){
 
   }
   
   ngOnInit(): void {
-    
+    this.getProductList();
+    this.dtOptions = {
+      data: this.productList,
+      columns: [{
+        title: 'Product Name',
+        data: 'p_name'
+      }, {
+        title: 'Product Desc',
+        data: 'p_description'
+      }, {
+        title: 'Price',
+        data: 'p_price'
+      }]
+    };
+  }
+
+  getProductList(){
+    this.productListSubscribe = this.productService.loadProducts().subscribe(res => {
+      this.productList = res;
+    });
   }
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    
+    production: false,
+    API_ENDPOINT: 'http://angular-crud.loc/api/'
 };
