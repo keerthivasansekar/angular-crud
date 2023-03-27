22d55ca42add014e8ad393c69d05cab3eb529249
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-03-27 18:39:39 +0530

    Service Creation

--- src/app/product/product-create/product-create.component.ts
+++ src/app/product/product-create/product-create.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+
+  constructor(private productService: ProductService){
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
 
 }
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  
+  constructor(private productService: ProductService){
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
