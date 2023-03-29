2bb4dc645e251cb0cb1383110f69f3662b786522
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-03-29 15:50:38 +0530

    Product update added

--- src/app/product/product-create/product-create.component.ts
+++ src/app/product/product-create/product-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -12,13 +12,23 @@ export class ProductCreateComponent implements OnInit {
 
   // @ts-ignore
   productForm: FormGroup;
+  productId: any;
 
   constructor(private productService: ProductService,
               private formBuilder: FormBuilder,
-              private router: Router){ }
+              private router: Router,
+              private activatedRoute: ActivatedRoute){ }
   
   ngOnInit(): void {
     this.createProductForm();
+    let productId = '';
+    if (this.activatedRoute.snapshot.params['productId']) {
+      productId = this.activatedRoute.snapshot.params['productId'];
+      if (productId !== '') {
+        this.loadProductDetails(productId);
+      }      
+    }
+
   }
 
   createProductForm(){
@@ -29,22 +39,36 @@ export class ProductCreateComponent implements OnInit {
     });
   }
 
-  createProduct(values: any, isUpdate: boolean){
+  createProduct(values: any){
     let formData = new FormData();
     formData.append('name', values.name);
     formData.append('description', values.description);
     formData.append('price', values.price);
 
-    if (isUpdate) {
-      //for update product
+    if (this.productId) {
+      formData.append('id', this.productId);
+      this.productService.updateProductInfo(formData).subscribe(res => {
+        if (res.result === 'success') {
+          this.router.navigate(['/product/product-list']);
+        }
+      });
     } else {
       this.productService.createProduct(formData).subscribe(res => {
         if (res.result === 'success') {
-          this.router.navigate(['/product/product-list'])
+          this.router.navigate(['/product/product-list']);
         }
       });
     }
 
+  }
+
+  loadProductDetails(productId: any){
+    this.productService.loadProductInfo(productId).subscribe(res => {
+      this.productForm.controls['name'].setValue(res.p_name);
+      this.productForm.controls['description'].setValue(res.p_description);
+      this.productForm.controls['price'].setValue(res.p_price);
+      this.productId = res.p_id;
+    });
   }
 
 
